f8e462137dd3f72035657066687ceb9af8215b39
Traxec <cui1320383665@gmail.com>
2017-10-21 11:53:22 +0800

    2017.10.21

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,17 +17,14 @@
     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
       <div class="carousel-caption">
         {{-- 导航栏 --}}
-        <nav class="navbar navbar-light navbar-fixed-top">
+        <nav class="navbar light navbar-fixed-top">
           <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Brand</a>
+              <a class="navbar-brand" href="{{route('welcome')}}"><img src="{{asset('./bootcss/home/images/Care.png')}}" alt=""></a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -69,72 +66,100 @@
               </ul>
             </div><!-- /.navbar-collapse -->
           </div><!-- /.container-fluid -->
-        </nav>      {{-- <nav class="navbar navbar-light navbar-fixed-top">
+        </nav>
+        {{-- <nav class="navbar navbar-light navbar-fixed-top">
 
-          <div class="nav pull-left">
-          <img src="{{asset('./bootcss/home/images/logo.jpg')}}" alt="care">
-        </div>
-        <ul class="nav nav-pills pull-right">
-        <li class="nav-item"><a class="nav-link" style="" href="{{ route('login') }}" role="tab" data-toggle="tab">Login</a></li>
-        <li class="nav-item"><a class="nav-link" style="" href="{{ route('register') }}" role="tab" data-toggle="tab">Register</a></li>
-      </ul>
-    </nav> --}}
+        <div class="nav pull-left">
+        <img src="{{asset('./bootcss/home/images/logo.jpg')}}" alt="care">
+      </div>
+      <ul class="nav nav-pills pull-right">
+      <li class="nav-item"><a class="nav-link" style="" href="{{ route('login') }}" role="tab" data-toggle="tab">Login</a></li>
+      <li class="nav-item"><a class="nav-link" style="" href="{{ route('register') }}" role="tab" data-toggle="tab">Register</a></li>
+    </ul>
+  </nav> --}}
 
-  </div>
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+</div>
+<!-- Indicators -->
+<ol class="carousel-indicators">
+  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+  <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+</ol>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="{{asset('./bootcss/home/images/1.jpg')}}" alt="...">
-      <div class="carousel-caption">
-      </div>
-    </div>
-    <div class="item">
-      <img src="{{asset('./bootcss/home/images/2.jpg')}}" alt="...">
-      <div class="carousel-caption">
-      </div>
-    </div>
-    <div class="item">
-      <img src="{{asset('./bootcss/home/images/3.jpg')}}" alt="...">
-      <div class="carousel-caption">
-      </div>
+<!-- Wrapper for slides -->
+<div class="carousel-inner" role="listbox">
+  <div class="item active">
+    <img src="{{asset('./bootcss/home/images/1.jpg')}}" alt="..." height="200">
+    <div class="carousel-caption">
     </div>
   </div>
+  <div class="item">
+    <img src="{{asset('./bootcss/home/images/2.jpg')}}" alt="...">
+    <div class="carousel-caption">
+    </div>
+  </div>
+  <div class="item">
+    <img src="{{asset('./bootcss/home/images/3.jpg')}}" alt="...">
+    <div class="carousel-caption">
+    </div>
+  </div>
+  <div class="item">
+    <img src="{{asset('./bootcss/home/images/4.jpg')}}" alt="...">
+    <div class="carousel-caption">
+    </div>
+  </div>
+  <div class="item">
+    <img src="{{asset('./bootcss/home/images/5.jpg')}}" alt="...">
+    <div class="carousel-caption">
+    </div>
+  </div>
+  <div class="item">
+    <img src="{{asset('./bootcss/home/images/6.jpg')}}" alt="...">
+    <div class="carousel-caption">
+    </div>
+  </div>
+</div>
 
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>  </div>
+<!-- Controls -->
+<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+</div>
 <script src="{{asset('./bootcss/home/js/jquery.min.js')}}"></script>
 <script src="{{asset('./bootcss/home/js/popper.min.js')}}"></script>
 <script src="{{asset('./bootcss/home/js/bootstrap.min.js')}}"></script>
 <script type="text/javascript">
+//轮播速率
 $('.carousel').carousel({
   interval: 2000,
 })
+//监听右侧下拉栏状态
 $(document).ready(function() {
   $(window).scroll(function() {
     if ($(document).scrollTop()<=0){
-      $('nav').removeClass('navbar-inverse')
+      $('nav').css('background','')
       $('nav').addClass('navbar-light')
     }else{
       $('nav').removeClass('navbar-light')
-      $('nav').addClass('navbar-inverse')
+      $('nav').css('background','-webkit-gradient(linear, 0 0, 0 bottom, from(pink), to(rgba(255,192,203,0.0))');
+      $('nav').css('border','0px');
     }
   });
 });
+//监听窗口改变
+$('.carousel-inner').css('height',$(window).height())
+$(window).resize(function(){
+  $('.carousel-inner').css('height',$(window).height())
+})
 </script>
 
 </body>
